cc005a39698ebdf6250102468ac928e871a78bba
Victor de Albuquerque Barros <vba002@prudentialdobrasil.com.br>
2019-07-29 11:48:13 -0300

    Primeiro teste

---
+++ AmbienteTeste/AmbienteTeste/SpecFlowFeature1.feature
@@ -0,0 +1,26 @@
+﻿Feature: Tela de Boas Vindas – Hub Simples.
+	Eu como franqueado
+	Desejo validar layout da tela de boas vindas.
+	A fim de ter acesso as plataformas vinculadas à jornada de vendas.
+
+
+@mytag
+Scenario:  Validar layout da tela de boas vindas.
+Given que um franqueado acesse a tela de login do aplicativo 
+When usuario acessar a tela de boas vindas.
+Then o acesso a tela inicial exibirá os ícones: Success Planner Digital, Plataforma de Vendas LP, Proposta Eletrônica e LP Online.
+
+Scenario: Acessar o link para o aplicativo Success Planner Digital.
+Given que um usuário tenha acesso ao App Success Planner.
+When ao clicar no link Success Planner.
+Then será redirecionado a tela inicial do Success Planner.
+
+Scenario: Acessar o link When o aplicativo Success Planner Digital já estiver sido instalado.
+Given que um usuário tenha o App Success Planner instalado.
+When ao clicar no link Success Planner.
+Then será apresentada mensagem Já instalado.
+
+Scenario: Acessar o link When o aplicativo Success Planner Digital  não estiver sido instalado.
+Given que um usuário não tenha o App Success Planner instalado.
+When ao clicar no link Success Planner.
+Then será apresentado um botão com o rótulo Instalar.
---
+++ AmbienteTeste/AmbienteTeste/TelaDeBoasVindasHubSimples_Steps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AmbienteTeste
+{
+    [Binding]
+    public class TelaDeBoasVindasHubSimples_Steps
+    {
+        [Given(@"que um franqueado acesse a tela de login do aplicativo")]
+        [Obsolete]
+        public void GivenQueUmFranqueadoAcesseATelaDeLoginDoAplicativo()
+        {
+           ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"que um usuário tenha acesso ao App Success Planner\.")]
+        [Obsolete]
+        public void GivenQueUmUsuarioTenhaAcessoAoAppSuccessPlanner_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"que um usuário tenha o App Success Planner instalado\.")]
+        [Obsolete]
+        public void GivenQueUmUsuarioTenhaOAppSuccessPlannerInstalado_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"que um usuário não tenha o App Success Planner instalado\.")]
+        [Obsolete]
+        public void GivenQueUmUsuarioNaoTenhaOAppSuccessPlannerInstalado_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"usuario acessar a tela de boas vindas\.")]
+        [Obsolete]
+        public void WhenUsuarioAcessarATelaDeBoasVindas_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"ao clicar no link Success Planner\.")]
+        [Obsolete]
+        public void WhenAoClicarNoLinkSuccessPlanner_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"o acesso a tela inicial exibirá os ícones: Success Planner Digital, Plataforma de Vendas LP, Proposta Eletrônica e LP Online\.")]
+        [Obsolete]
+        public void ThenOAcessoATelaInicialExibiraOsIconesSuccessPlannerDigitalPlataformaDeVendasLPPropostaEletronicaELPOnline_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"será redirecionado a tela inicial do Success Planner\.")]
+        [Obsolete]
+        public void ThenSeraRedirecionadoATelaInicialDoSuccessPlanner_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"será apresentada mensagem “Já instalado”\.")]
+        [Obsolete]
+        public void ThenSeraApresentadaMensagemJaInstalado_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"será apresentado um botão com o rótulo “Instalar”\.")]
+        [Obsolete]
+        public void ThenSeraApresentadoUmBotaoComORotuloInstalar_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
